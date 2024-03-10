2e88e4a3e8f303aa3e5024d2bbc7578451c5fc9c
xaviers-sandbox <craps@crapss-mbp.attlocal.net>
2024-03-09 23:21:09 -0500

    aws reference app initial commit

---
+++ aws-reference-app/src/main/java/com/aws/playground/AwsReferenceApp.java
@@ -0,0 +1,13 @@
+package com.aws.playground;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsReferenceApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsReferenceApp.class, args);
+	}
+
+}
